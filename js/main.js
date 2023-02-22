e48a785bc4a992d4262e0b17d2ed9b701b246c4b
AngeloRoudani <ange.rouda@gmail.com>
2023-02-22 16:54:30 +0100

    aggiunta prezzo scontato

--- js/main.js
+++ js/main.js
@@ -27,11 +27,13 @@ let scontoQuaranta = (totTemp - ((totTemp / 100) * 40)).toFixed(2);
 if (nEta < 18) {
 
     console.log((scontoVenti));
+    document.getElementById('my_price_old').innerHTML = totTemp;
     document.getElementById('my_price').innerHTML = scontoVenti + " (-20%)";
 
 } else if (nEta >= 65) {
 
     console.log(scontoQuaranta);
+    document.getElementById('my_price_old').innerHTML = totTemp;
     document.getElementById('my_price').innerHTML = scontoQuaranta + " (-40%)";
 
 } else  {
